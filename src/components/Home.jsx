6d84e8b4926bfcc9a3e2b60860dd1dcd5dd19874
Jack Badger <jackbadger16@gmail.com>
2024-03-03 08:54:31 +0000

    Portfolio 1

---
+++ src/components/Home.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* container */}
+
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full ">
+        <p className="text-pink-600">Hi, my name is</p>
+        <h1 className="text-4xl md:text-7xl font-bold text-[#ccd6f6]">
+          Jack Badger
+        </h1>
+        <h2 className="text-4xl md:text-7xl font-bold text-[#8892b0]">
+          I'm a Junior Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px] ">
+          I'm a Junior Developer looking to change careers from a
+          Physiotherapist to a Front-End Developer. I'm currently focused on
+          building two projects one centering around logging rock climbs, ther
+          other centering around an ingrediants application.
+        </p>
+        <div>
+          <button className="text-white group border-2 rounded px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600 ">
+          View Work 
+            <span className="group-hover:rotate-90 duration-300">
+            <HiArrowNarrowRight className="ml-3" />
+            </span>
+            
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
